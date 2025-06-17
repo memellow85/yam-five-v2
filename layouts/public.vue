14991d560f93017e8d3cc9403538716469a81835
Emanuele Barachetti <emanuele.barachetti@serioplast.com>
2025-06-17 13:44:31 +0200

    primo deploy

---
+++ layouts/public.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import Package from '@@/package.json'
+const pkg = Package
+</script>
+
+<template>
+  <div>
+    <header>header</header>
+    <main>
+      <slot />
+    </main>
+    <footer class="p-3">
+      <p class="text-xs text-center yf-text-base">
+        {{ $t('copyright') }} &copy; {{ new Date().getFullYear() }} - {{ $t('versions') }}:
+        {{ pkg.version }}
+      </p>
+    </footer>
+  </div>
+</template>
