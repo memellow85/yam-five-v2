d798f790149320bef98cf4d088f5d1e1c445b2f4
Emanuele Barachetti <emanuele.barachetti@serioplast.com>
2025-07-03 15:33:43 +0200

    test settings pwa

--- layouts/public.vue
+++ layouts/public.vue
@@ -9,56 +9,14 @@ const loaderStore = useMyLoaderStore()
 <template>
   <div class="h-dvh w-dvw grid grid-cols-1 place-items-center overflow-hidden bg-slate-100">
     <div class="w-96 p-3 grid grid-cols-1">
+      {{ $pwa }}
       <template v-if="!loaderStore.show">
         <header>
           <h1 class="font-special text-center text-3xl yf-text-base">
             {{ $t('name_app') }}
           </h1>
         </header>
-        <main class="pt-9 pb-3">
-          {{ $pwa }}
-          <ClientOnly>
-            <div
-              v-if="$pwa?.offlineReady || $pwa?.needRefresh"
-              class="pwa-toast"
-              role="alert"
-            >
-              <div class="message">
-                <span v-if="$pwa.offlineReady">
-                  App ready to work offline
-                </span>
-                <span v-else>
-                  New content available, click on reload button to update.
-                </span>
-              </div>
-              <button
-                v-if="$pwa.needRefresh"
-                @click="$pwa.updateServiceWorker()"
-              >
-                Reload
-              </button>
-              <button @click="$pwa.cancelPrompt()">
-                Close
-              </button>
-            </div>
-            <div
-              v-if="$pwa?.showInstallPrompt && !$pwa?.offlineReady && !$pwa?.needRefresh"
-              class="pwa-toast"
-              role="alert"
-            >
-              <div class="message">
-                <span>
-                  Install PWA
-                </span>
-              </div>
-              <button @click="$pwa.install()">
-                Install
-              </button>
-              <button @click="$pwa.cancelInstall()">
-                Cancel
-              </button>
-            </div>
-          </ClientOnly>
+        <main class="pt-9 pb-3">        
           <slot />
         </main>
         <footer>
